a72fe94a0987412922689657cf466f0cde6f0d7f
fenrirsong <fenrirsong@users.noreply.github.com>
2016-04-20 17:41:29 -0700

    XBox talks to Computer
    This code is in progress.  So far, the XBox controller talks to the computer pretty nicely.  Now to hook up an Arduino, Zaber and Thorlabs actuators.

---
+++ XBoxStage-master/XBoxStage/Settings.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace XBoxStage
+{
+    // Put all values to be saved between sessions into this object
+    class Settings
+    {
+        private const string filename = @"settings.json";
+
+        public Point pointA { get; set; }
+        public Point pointB { get; set; }
+
+        public static void Save (Settings settings)
+        {
+            File.WriteAllText(filename, JsonConvert.SerializeObject(settings, Formatting.Indented));
+        }
+        public static Settings Restore()
+        {
+            if (File.Exists(filename))
+            {
+                return JsonConvert.DeserializeObject<Settings>(File.ReadAllText(filename)); ;
+            }
+            return new Settings();
+        }
+    }
+}
